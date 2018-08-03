573155f18412a18cf8df6e45f7d44fbf68dfece0
deep-introspection <deep@introspection.eu>
2018-08-03 16:08:53 +0200

    Paris hacksession + Tshirt bonus

--- data/__init__.py
+++ data/__init__.py
@@ -8,7 +8,7 @@ datafolder = Path('data/')
 n = len(sorted(list(datafolder.glob(pattern='*RS.bdf'))))
 
 # Generator for the raw files
-def raws():
+def raws(verbose=False):
 
     datafiles = sorted(list(datafolder.glob(pattern='*RS.bdf')))
 
@@ -25,34 +25,50 @@ def raws():
         
         raw.info['subject_info'] = {'pid': f.name[7:11],
                                     'group': f.name[3:6]}
-        
-        print('subject: {id}'.format(id=f.name[7:11]))
+        if verbose:
+            print('subject: {id}'.format(id=f.name[7:11]))
 
         yield raw
 
 
 # Generator for the events
 def events():
-
     for raw in raws():
         # find the events in the raw data
-        #eventarray = mne.find_events(raw, verbose="ERROR")
-        eventarray = mne.find_events(raw, stim_channel='Status', verbose="ERROR",output='step', uint_cast=True)
-        # throw warning if the events seem off
-        if eventarray.shape[0] != 4:
-            warnings.warn("Expected 4 events, but got {events}".format(events=eventarray.shape[0]))
+        ev = mne.find_events(raw, verbose="ERROR")
+        
+        sf = raw.info['sfreq']
 
+        #ev = mne.find_events(raw, stim_channel='Status', verbose="ERROR", output='step', uint_cast=True)
+        # throw warning if the events seem off
+        if ev.shape[0] != 4:
+            warnings.warn("Expected 4 events, but got {events}".format(events=ev.shape[0]))
+
+        print(f"First event at {ev[0][0]/sf} sec")
+        
+        eventarray = []
+        et = ev[0][0]
+        eyes_flag = 101
+        
+        while (et+60*sf)<len(raw):
+            eventarray.append([et, 0, eyes_flag])
+            if eyes_flag == 101:
+                eyes_flag = 102
+            else:
+                eyes_flag = 101
+            et = et + 60 * sf
+        
         # manually fix issues with the triggers
         # 101 = eyes open, 102 = eyes closed
-        eventarray[:, 1] = 0
-        if eventarray.shape[0] == 4:
-            eventarray[:, 2] = [101, 102, 101, 102]
-        elif eventarray.shape[0] == 5:
-            eventarray[:, 2] = [101, 102, 101, 102, 101]
-        elif eventarray.shape[0] == 6:
-            eventarray[:, 2] = [101, 102, 101, 102, 101, 102]  
+        # eventarray[:, 1] = 0
+        # if eventarray.shape[0] == 4:
+        #     eventarray[:, 2] = [101, 102, 101, 102]
+        # elif eventarray.shape[0] == 5:
+        #     eventarray[:, 2] = [101, 102, 101, 102, 101]
+        # elif eventarray.shape[0] == 6:
+        #     eventarray[:, 2] = [101, 102, 101, 102, 101, 102]  
 
-        yield eventarray
+        yield np.array(eventarray)
 
 
 
